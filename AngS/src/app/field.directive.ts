99a1587e3a36920877c25e4bbbbd6ed36afc951e
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-10-26 18:49:22 -0300

    .

--- AngS/src/app/field.directive.ts
+++ AngS/src/app/field.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[eveField]'
 })
-export class FieldDirective {
+export class FieldDirective implements OnInit {
 
-  constructor() { }
+  constructor(
+    private campo:ElementRef
+  ) { }
 
+
+  ngOnInit():void {
+    const campo = this.campo.nativeElement;
+
+    if(campo.name){
+      campo.settAtribute('placeholder',`digite o ${campo.name}`);
+    }else{
+      throw new Error("atributo 'name' é obrigatório");
+    }
+
+
+  }
 }
