432ebe8ae4eacc9b29b68c52d4b3e50ff1903566
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-10-26 20:43:15 -0300

    aplicando conceitos da arquitetura LIFT

--- AngS/src/app/app.module.ts
+++ AngS/src/app/app.module.ts
@@ -3,20 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SearchComponent } from './header/search/search.component';
 import { FieldDirective } from './field.directive';
+import { HomeComponent } from './pages/home/home.component';
+import { AtaqueComponent } from './pages/ataque/ataque.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SearchComponent,
     FieldDirective,
+    HomeComponent,
+    AtaqueComponent,
+    CadastroComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
